db03527bf8a3cf6726b4faef4e78e47f5f584a8a
Frank Zhiming Xue <ifrankxue@gmail.com>
2024-10-24 20:14:09 -0700

    Update the user-profile: view, edit, and delete.

--- a_posts/models.py
+++ a_posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 
@@ -9,6 +10,7 @@ class Post(models.Model):
     artist = models.CharField(max_length=500, null=True)
     url = models.URLField(max_length=500, null=True)
     image = models.URLField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='posts')
     body = models.TextField()
     tags = models.ManyToManyField('Tag')
     created = models.DateTimeField(auto_now_add=True)
--- a_users/urls.py
+++ a_users/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', profile_view, name='profile'),
     path('edit/', profile_edit_view, name='profile-edit'),
+    path('delete/', profile_delete_view, name='profile-delete'),
+    path('<str:username>/', profile_view, name='user-profile'),
 ]
